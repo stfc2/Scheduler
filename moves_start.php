4d9c87a33eea1fb53675d4691169e0bb1eddd8c4
merry75 <kirirur@gmail.com>
2008-05-23 12:45:31 +0000

    Modificato il demone in  modo da usare le classi definite in commons.php e lo script di config al posto degli URL cablati.
    Tradotto in inglese i commenti ed i messaggi di log.

--- moves_start.php
+++ moves_start.php
@@ -20,163 +20,26 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+
 // ########################################################################################
 // ########################################################################################
 // Startup Konfig
+
+// include game definitions, path url and so on
+include('config.script.php');
 
 error_reporting(E_ALL);
 ini_set('memory_limit', '600M');
 
 if(!empty($_SERVER['SERVER_SOFTWARE'])) {
-    echo 'Der Scheduler kann nur per CLI aufgerufen werden!'; exit;
+    echo 'The scheduler can only be called by CLI!'; exit;
 }
 
-define('TICK_LOG_FILE', '|script_dir|/game/logs/moves_tick_'.date('d-m-Y', time()).'.log');
+define('TICK_LOG_FILE', $game_path . 'game/logs/moves_tick_'.date('d-m-Y', time()).'.log');
 define('IN_SCHEDULER', true); // wir sind im scheduler...
 
-
-// ########################################################################################
-// ########################################################################################
-// App-Klasse (im Moment nur Logging/Timing)
-
-class scheduler {
-    var $start_values = array();
-
-    function log($message) {
-        $fp = fopen(TICK_LOG_FILE, 'a');
-        fwrite($fp, $message."\n");
-        echo str_replace('\n','<br>',$message.'\n');
-        fclose($fp);
-    }
-
-    function start_job($name) {
-        global $db;
-
-        $this->log('<font color=#0000ff>Starting <b>'.$name.'</b>...</font>');
-
-        $this->start_values[$name] = array( time() + microtime() , $db->i_query );
-    }
-
-    function finish_job($name) {
-        global $db;
-
-        $this->log('<font color=#0000ff>Executed <b>'.$name.'</b> (</font><font color=#ff0000>queries: '.($db->i_query - $this->start_values[$name][1]).'</font><font color=#0000ff>) in </font><font color=#009900>'.round( (time() + microtime()) - $this->start_values[$name][0] , 4).' secs</font><br>');
-
-    }
-}
-
-
-// ########################################################################################
-// ########################################################################################
-// andere Funktionen
-
-function RetrieveRealCatId($id,$user_id)
-{
-global $db,$sdl;
-$player=$db->queryrow('SELECT user_race FROM user WHERE user_id="'.$user_id.'"');
-$catquery=$db->query('SELECT id FROM ship_ccategory WHERE race="'.$player['user_race'].'"');
-$cat_id=1;
-while (($cat=$db->fetchrow($catquery))==true)
-{
-if ($cat['id']==$id) return $cat_id;
-$cat_id++;
-}
-return -1;
-}
-
-
-
-
-
-
-function GetBuildingTimeTicks($building,$planet,$user_race)
-{
-global $db;
-global $RACE_DATA, $BUILDING_NAME, $BUILDING_DATA, $MAX_BUILDING_LVL,$NEXT_TICK,$ACTUAL_TICK,$PLANETS_DATA;
-
-$time=($BUILDING_DATA[$building][3] + 3*pow($planet['building_'.($building+1)],$BUILDING_DATA[$building][4]));
-if ($building==9)
-	$time=$BUILDING_DATA[$building][3];
-$time*=$RACE_DATA[$user_race][1];
-$time/=100;
-$time*=(100-2*($planet['research_4']*$RACE_DATA[$user_race][20]));
-$time*=$PLANETS_DATA[$planet['planet_type']][5];
-$time=round($time,0);
-
-return $time;
-}
-
-
-
-function UnitTimeTicksScheduler($unit,$planet_research,$user_race)
-{
-global $RACE_DATA, $UNIT_NAME, $UNIT_DATA, $MAX_BUILDING_LVL,$NEXT_TICK,$ACTUAL_TICK;
-if ($unit<0 || $unit>5) return 0;
-
-$time=$UNIT_DATA[$unit][4];
-$time*=$RACE_DATA[$user_race][2];
-$time/=100;
-$time*=(100-2*($planet_research*$RACE_DATA[$user_race][20]));
-
-if ($time<1) $time=1;
-$time=round($time,0);
-return $time;
-}
-
-
-
-function ResourcesPerTickMetal($planet) {
-	$rid=0;
-    global $RACE_DATA,$PLANETS_DATA, $addres;
-    $result = 0.25*(3*((pow(((3*$PLANETS_DATA[$planet['planet_type']][$rid])*(1+$planet['building_'.($rid+2)])),1.35))/100*(50+ (50*(1/($planet['building_'.($rid+2)]*100+100))*($planet['workermine_'.($rid+1)]+100))))*($RACE_DATA[$planet['user_race']][9+$rid]*($addres[$planet['research_5']]*$RACE_DATA[$planet['user_race']][20])));
-	if($result < 10) $round = 1;
-    else $round = 0;
-    return round($result, $round);
-}
-
-function ResourcesPerTickMineral($planet) {
-	$rid=1;
-    global $RACE_DATA,$PLANETS_DATA, $addres;
-    $result = 0.25*(3*((pow(((3*$PLANETS_DATA[$planet['planet_type']][$rid])*(1+$planet['building_'.($rid+2)])),1.35))/100*(50+ (50*(1/($planet['building_'.($rid+2)]*100+100))*($planet['workermine_'.($rid+1)]+100))))*($RACE_DATA[$planet['user_race']][9+$rid]*($addres[$planet['research_5']]*$RACE_DATA[$planet['user_race']][20])));
-    if($result < 10) $round = 1;
-    else $round = 0;
-    return round($result, $round);
-}
-
-function ResourcesPerTickLatinum($planet) {
-	$rid=2;
-    global $RACE_DATA,$PLANETS_DATA, $addres;
-    $result = 0.2*(3*((pow(((3*$PLANETS_DATA[$planet['planet_type']][$rid])*(1+$planet['building_'.($rid+2)])),1.35))/100*(50+ (50*(1/($planet['building_'.($rid+2)]*100+100))*($planet['workermine_'.($rid+1)]+100))))*($RACE_DATA[$planet['user_race']][9+$rid]*($addres[$planet['research_5']]*$RACE_DATA[$planet['user_race']][20])));
-    if($result < 10) $round = 1;
-    else $round = 0;
-    return round($result, $round);
-}
-
-
-
-function UnitPrice($unit,$resource, $race)
-{
-global $db;
-global $RACE_DATA, $UNIT_NAME, $UNIT_DATA, $MAX_BUILDING_LVL,$NEXT_TICK,$ACTUAL_TICK;
-if ($unit<0 || $unit>5) return 0;
-
-$price = $UNIT_DATA[$unit][$resource];
-$price*= $RACE_DATA[$race][6];
-return round($price,0);
-}
-
-
-function GetTaxes($percent,$alliance_id)
-{
-global $db;
-
-if ($percent<=0) return (array(0,0,0));
-$res=$db->queryrow('SELECT SUM(p.add_1/100*'.$percent.') AS r1,SUM(p.add_2/100*'.$percent.') AS r2,SUM(p.add_3/100*'.$percent.') AS r3 FROM user u LEFT join planets p ON p.planet_owner=u.user_id WHERE (u.user_alliance="'.$alliance_id.'") AND (u.user_active=1)');
-$res[0]=round($res['r1']);
-$res[1]=round($res['r2']);
-$res[2]=round($res['r3']);
-return $res;
-}
+// include commons classes and functions
+include('commons.php');
 
 
 // ########################################################################################
@@ -185,13 +48,13 @@ return $res;
 
 $starttime = ( microtime() + time() );
 
-include('|script_dir|/game/include/sql.php');
-include('|script_dir|/game/include/global.php');
-include('|script_dir|/game/include/functions.php');
-include('|script_dir|/game/include/text_races.php');
-include('|script_dir|/game/include/race_data.php');
-include('|script_dir|/game/include/ship_data.php');
-include('|script_dir|/game/include/libs/moves.php');
+//include($game_path . 'game/include/sql.php');
+include($game_path . 'game/include/global.php');
+include($game_path . 'game/include/functions.php');
+include($game_path . 'game/include/text_races.php');
+include($game_path . 'game/include/race_data.php');
+include($game_path . 'game/include/ship_data.php');
+include($game_path . 'game/include/libs/moves.php');
 
 $sdl = new scheduler();
 $db = new sql($config['server'].":".$config['port'], $config['game_database'], $config['user'], $config['password']); // create sql-object for db-connection
@@ -220,15 +83,15 @@ if(empty($ACTUAL_TICK)) {
 }
 
 /*
-Beispiel Job:
+Example Job:
 
-$sdl->start_job('Mein Job');
+$sdl->start_job('Mine Job');
 
-mach irgendwas...bei Fehlern/Meldung:
+do something ... during error / message:
   $sdl->log('...');
-am besten mit - davor, damit es sich von den anderen nachrichten abhebt, also: $sdl->log('- hier stimmt was nicht');
+best also - before, so it's apart from the other messages, also: $sdl->log('- this was not true');
 
-$sdl->finish_job('Mein Job'); // beendet den timer
+$sdl->finish_job('Mine Job'); // terminates the timer
 
  */
 
@@ -244,8 +107,9 @@ $sdl->finish_job('Moves Scheduler');
 
 // ########################################################################################
 // ########################################################################################
-// Beenden und Log schließen
+// Quit and close log
 
+$db->close();
 $sdl->log('<b>Finished Scheduler in <font color=#009900>'.round((microtime()+time())-$starttime, 4).' secs</font>'."\n".'Executed Queries: <font color=#ff0000>'.$db->i_query.'</font></b>');
 
 ?>
