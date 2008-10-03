0883fc71f479b6266048d3b1d2bb38a50d53aec4
merry75 <kirirur@gmail.com>
2008-10-03 15:09:01 +0000

    Allineati i file al nuovo dominio.

--- config.script.php
+++ config.script.php
@@ -1,13 +1,13 @@
 <?
 
 // This should point directly to the directory game.
-$game_path = '/home/taku/public_html/stfc/game/';
+$game_path = '/root/stfc/game/';
 
 // This should point to the directory where the daemons game are installed.
 $script_path = '/home/taku/stfc/';
 
 // This should point to the external url of the game.
-$game_url = 'http://stfc.nonsolotaku.it';
+$game_url = 'http://www.stfc.it';
 
  
 ?>
