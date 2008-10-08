3395f7e86e297e312fcd731ab6088e781ae622cf
merry75 <kirirur@gmail.com>
2008-10-08 13:23:21 +0000

    Aggiunti due messaggi di rapporto sulle operazioni relative alle Rotte Commerciali: uno al proprietario della flotta, se abilitato e l'altro al proprietario del pianeta se diverso da quello della flotta.

--- moves_action_34.php
+++ moves_action_34.php
@@ -23,27 +23,26 @@
 class moves_action_34 extends moves_common {
 
     var $tr_data = array();
-
-
-
     var $fleet = array();
-
-
-
     var $actions;
-
-
+    var $unlwares = array();    // unloaded wares
+    var $lwares = array();      // loaded wares
+    var $planet_overloaded;     // space still available on planet?
 
     function do_unloading() {
 
-        $this->log(MV_M_NOTICE, 'Unloading goods...');
+        //$this->log(MV_M_NOTICE, 'Unloading goods...');
 
         $wares = array(201 => 'resource_1', 202 => 'resource_2', 203 => 'resource_3', 204 => 'resource_4', 211 => 'unit_1', 212 => 'unit_2', 213 => 'unit_3', 214 => 'unit_4', 215 => 'unit_5', 216 => 'unit_6');
+        $i = 0;
+        $this->planet_overloaded = false;
 
         foreach($wares as $code => $column) {
 
             if($this->actions[$code] == 0) {
                 //$this->log(MV_M_NOTICE, 'Nulla da scaricare per quanto riguarda '.$column);
+                $this->unlwares[$i] = 0;
+                $i++;
                 continue;
             }
 
@@ -52,6 +51,7 @@ class moves_action_34 extends moves_common {
                 if($this->fleet[$column] > 0)
                     $this->log(MV_M_NOTICE, 'Unloading <b>ALL</b> the quantity on board of '.$column.' (<b>'.$this->fleet[$column].'</b>)');
                 $this->dest[$column] += $this->fleet[$column];
+                $this->unlwares[$i] = $this->fleet[$column];
                 $this->fleet[$column] = 0;
             }
 
@@ -61,8 +61,9 @@ class moves_action_34 extends moves_common {
                     $this->log(MV_M_NOTICE, 'Unloading <b>'.$value.'</b> of '.$column);
                 $this->dest[$column] += $value;
                 $this->fleet[$column] -= $value;
+                $this->unlwares[$i] = $value;
             }
-
+            $i++;
         }
 
     }
@@ -71,20 +72,15 @@ class moves_action_34 extends moves_common {
 
     function do_loading() {
 
-        // von ship_traderoute.php...und das wahrscheinlich von ship_fleets_loadingp/f
+        // from ship_traderoute.php... and probably from ship_fleets_loadingp/f
 
-        $this->log(MV_M_NOTICE, 'Loading goods...');
+        //$this->log(MV_M_NOTICE, 'Loading goods...');
 
         $n_resources = $this->fleet['resource_1'] + $this->fleet['resource_2'] + $this->fleet['resource_3'];
-
         $n_units = $this->fleet['resource_4'] + $this->fleet['unit_1'] + $this->fleet['unit_2'] + $this->fleet['unit_3'] + $this->fleet['unit_4'] + $this->fleet['unit_5'] + $this->fleet['unit_6'];
-
-
-
         $resources = array(101 => 'resource_1', 102 => 'resource_2', 103 => 'resource_3');
-
         $units = array(104 => 'resource_4', 111 => 'unit_1', 112 => 'unit_2', 113 => 'unit_3', 114 => 'unit_4', 115 => 'unit_5', 116 => 'unit_6');
-
+        $i = 0;
 
 
         foreach($resources as $code => $column) {
@@ -105,9 +101,10 @@ class moves_action_34 extends moves_common {
             $this->fleet[$column] += $value;
             $this->report_load[$column] = $value;
             $this->dest[$column] -= $value;
+            $this->lwares[$i] = $value;
 
             $n_resources += $value;
-
+            $i++;
         }
 
 
@@ -130,7 +127,10 @@ class moves_action_34 extends moves_common {
             $this->fleet[$column] += $value;  
             $this->report_load[$column] = $value;
             $this->dest[$column] -= $value;
+            $this->lwares[$i] = $value;
+
             $n_units += $value;
+            $i++;
         }
     }
 
@@ -138,55 +138,31 @@ class moves_action_34 extends moves_common {
     function _action_main() {
 
         if($this->flags['combat_happened']) {
-
             $sql = 'UPDATE ship_fleets
-
                     SET planet_id = '.$this->move['dest'].',
-
                         move_id = 0
-
                     WHERE fleet_id = '.$this->fleet_ids[0];
 
-
-
             if(!$this->db->query($sql)) {
-
                 return $this->log(MV_M_DATABASE, 'Could not update ships position data');
-
             }
 
-
-
             return MV_EXEC_OK;
-
         }
-
 
 
         if($this->flags['free_dest_planet']) {
-
             $sql = 'UPDATE ship_fleets
-
                     SET planet_id = '.$this->move['dest'].',
-
                         move_id = 0
-
                     WHERE fleet_id = '.$this->fleet_ids[0];
 
-
-
             if(!$this->db->query($sql)) {
-
                 return $this->log(MV_M_DATABASE, 'Could not update ships position data');
-
             }
 
-
-
             return MV_EXEC_OK;
-
         }
-
 
 
         $this->tr_data = $this->action_data;
@@ -195,155 +171,88 @@ class moves_action_34 extends moves_common {
 
         if($this->tr_data[5] < 0) $this->tr_data[5] = -1;
 
-
+        // On which planet we are arrived?
         if($this->move['dest'] == $this->tr_data[2]) $this->actions = &$this->tr_data[4];
-
         else $this->actions = &$this->tr_data[3];
 
         $this->log(MV_M_NOTICE, 'Buondi! Sono quello della flotta <b>'.$this->fleet_ids[0].'</b> del Boss <b>'.$this->move['user_id'].'</b>, in arrivo sul pianeta <b>'.$this->dest['planet_id'].'</b> che appartiene a <b>'.$this->dest['user_id'].'</b> per un trasporto.');
 
         $sql = 'SELECT resource_1, resource_2, resource_3, resource_4, unit_1, unit_2, unit_3, unit_4, unit_5, unit_6
-
                 FROM ship_fleets
-
                 WHERE fleet_id = '.$this->fleet_ids[0];
 
-
-
         if(($this->fleet = $this->db->queryrow($sql)) === false) {
-
             return $this->log(MV_M_DATABASE, 'Could not query fleet data');
-
         }
 
-
-
         $sql = 'SELECT COUNT(s.ship_id) AS n_transporter
-
                 FROM (ships s, ship_templates st)
-
                 WHERE s.fleet_id = '.$this->fleet_ids[0].' AND
-
                       st.id = s.template_id AND
-
                       st.ship_torso = '.SHIP_TYPE_TRANSPORTER;
 
-
-
         if(($n_transporter = $this->db->queryrow($sql)) === false) {
-
             return $this->log(MV_M_DATABASE, 'Could not query n_transporter data');
-
         }
 
 
 
         $this->fleet['max_resources'] = $n_transporter['n_transporter'] * MAX_TRANSPORT_RESOURCES;
-
         $this->fleet['max_units'] = $n_transporter['n_transporter'] * MAX_TRANSPORT_UNITS;
 
 
 
         $this->do_unloading();
 
-        //$this->log(MV_M_NOTICE, 'Ho finito di scaricare');
-
 
         if($this->move['user_id'] != $this->dest['user_id']) {
-
-//            $this->log(MV_M_NOTICE, 'Devo decidere se sei un mio alleato');
-
             $sql = 'SELECT ud.ud_id, a.alliance_id, ad.ad_id, ad.type, ad.status 
-
                     FROM (user u)
-
                     LEFT JOIN user_diplomacy ud ON ( ( ud.user1_id = '.$this->move['user_id'].' AND ud.user2_id = '.$this->dest['user_id'].' ) OR (ud.user1_id = '.$this->dest['user_id'].' AND ud.user2_id = '.$this->move['user_id'].' ) )
-
                     LEFT JOIN alliance a ON a.alliance_id = u.user_alliance
-
                     LEFT JOIN alliance_diplomacy ad ON ( ( ad.alliance1_id = '.$this->move['user_alliance'].' AND ad.alliance2_id = a.alliance_id ) OR ( ad.alliance1_id = a.alliance_id AND ad.alliance2_id = '.$this->move['user_alliance'].' ) )
-
                     WHERE u.user_id = '.$this->dest['user_id'];
 
-
-//            $this->log(MV_M_NOTICE, 'Query per alleanza: '.$sql);
-
-
             if(($diplomacy = $this->db->queryrow($sql)) === false) {
-
                 return $this->log(MV_M_DATABASE, 'Could not query diplomacy data');
-
             }
-
-//            $this->log(MV_M_NOTICE, 'Ho raccolto i dati sulle alleanze, ora controllo...');
 
             $allied = false;
 
 
             if(!empty($diplomacy['ud_id'])) $allied = true;
 
-
             if( ($diplomacy['alliance_id'] != 0) && ($diplomacy['alliance_id'] == $this->move['user_alliance']) ) $allied = true;
 
-
             if(!empty($diplomacy['ad_id'])) {
-
                 if($diplomacy['type'] == 2) {
-
                     if($diplomacy['status'] == 0) $allied = true;
-
                 }
-
             }
-
         }
 
         else {
-
             $allied = true;
-
         }
-
-//        $this->log(MV_M_NOTICE, 'Ho deciso se sei un alleato o no...');
 
         if($allied && ($this->tr_data[5] < 0 || $this->tr_data[5] == 4)) $this->do_loading();
 
-        //$this->log(MV_M_NOTICE, 'Nel caso, ho finito di caricare');
-
-
         $sql = 'UPDATE ship_fleets
-
                 SET resource_1 = '.$this->fleet['resource_1'].',
-
                     resource_2 = '.$this->fleet['resource_2'].',
-
                     resource_3 = '.$this->fleet['resource_3'].',
-
                     resource_4 = '.$this->fleet['resource_4'].',
-
                     unit_1 = '.$this->fleet['unit_1'].',
-
                     unit_2 = '.$this->fleet['unit_2'].',
-
                     unit_3 = '.$this->fleet['unit_3'].',
-
                     unit_4 = '.$this->fleet['unit_4'].',
-
                     unit_5 = '.$this->fleet['unit_5'].',
-
                     unit_6 = '.$this->fleet['unit_6'].'
-
                 WHERE fleet_id = '.$this->fleet_ids[0];
 
-
-
         if(!$this->db->query($sql)) {
-
-            return $this->log(MV_M_DATABASE, 'Could not update ship fleets resource data');	    
-
+            return $this->log(MV_M_DATABASE, 'Could not update ship fleets resource data');
         }
-
-
 
         $sql = 'UPDATE planets
                 SET resource_1 = '.$this->dest['resource_1'].',
@@ -358,15 +267,9 @@ class moves_action_34 extends moves_common {
                     unit_6 = '.$this->dest['unit_6'].'
                 WHERE planet_id = '.$this->dest['planet_id'];
 
-//        $this->log(MV_M_NOTICE, 'SQL UPDATE del pianeta: '.$sql);
-
-
         if(!$this->db->query($sql)) {
-
             return $this->log(MV_M_DATABASE, 'Could not update planets resource data');
-
         }
-
 
 
 	if($this->tr_data[5] < 0 || $this->tr_data[5] == 4) {
@@ -400,10 +303,55 @@ class moves_action_34 extends moves_common {
 		}
 	}
 
+	// #############################################################################
+	// Logbook entry
+
+	$sql = 'SELECT st.name, st.ship_torso, st.race,
+	               COUNT(s.ship_id) AS n_ships
+	        FROM ship_templates st, ship_fleets f, ships s
+	        WHERE f.fleet_id IN ('.$this->fleet_ids_str.') AND
+	              s.template_id = st.id AND
+	              s.fleet_id = f.fleet_id
+	        GROUP BY st.id
+	        ORDER BY st.ship_torso ASC, st.race ASC';
+
+	if(!$q_stpls = $this->db->query($sql)) {
+		return $this->log(MV_M_DATABASE, 'Could not query ships templates data! SKIP');
+	}
+
+	$log_data = array(34, $this->move['user_id'], $this->move['start'], $this->start['planet_name'], $this->start['user_id'], $this->move['dest'], $this->dest['planet_name'], $this->dest['user_id'], array(), $this->unlwares, $this->lwares, $this->planet_overloaded);
+
+	while($stpl = $this->db->fetchrow($q_stpls)) {
+		$log_data[8][] = array($stpl['name'], $stpl['ship_torso'], $stpl['race'], $stpl['n_ships']);
+	}
+
+	// #############################################################################
+	// Retrieve player language
+	switch($this->move['language'])
+	{
+		case 'GER':
+			$log_title1 = 'Transport bei '.$this->dest['planet_name'].' durchgef&uuml;hrt';
+			$log_title2 = 'Transport bei '.$this->dest['planet_name'].' erhalten';
+		break;
+		case 'ITA':
+			$log_title1 = 'Trasporto per '.$this->dest['planet_name'].' compiuto';
+			$log_title2 = 'Trasporto per '.$this->dest['planet_name'].' ricevuto';
+		break;
+		default:
+			$log_title1 = 'Transport for '.$this->dest['planet_name'].' accomplished';
+			$log_title2 = 'Transport for '.$this->dest['planet_name'].' received';
+		break;
+	}
+
+	// Add logbook entries only if players had activated them
+	if($this->action_data[6])
+		add_logbook_entry($this->move['user_id'], LOGBOOK_TACTICAL, $log_title1, $log_data);
+
+	// Only one logbook if the owner of the planet is also the owner of the fleets
+	if($this->move['user_id'] != $this->dest['user_id'])
+		add_logbook_entry($this->dest['user_id'], LOGBOOK_TACTICAL, $log_title2, $log_data);
+
 	return MV_EXEC_OK;
-
-
-
     }
 
 }
