67e6c72315e88e08cf13e11bd9c8f9ea98c42465
merry75 <kirirur@gmail.com>
2013-01-27 10:41:21 +0000

    Added correct php header to the file.
    Corrected script configuration according to current server configuration.

--- config.script.php
+++ config.script.php
@@ -1,10 +1,10 @@
-<?
+<?php
 
 // This should point directly to the directory game.
-$game_path = '/home/taku/public_html/game/';
+$game_path = '/home/stfc/public_html/game/';
 
 // This should point to the directory where the daemons game are installed.
-$script_path = '/home/taku/stfc/';
+$script_path = '/home/stfc/daemons/';
 
 // This should point to the external url of the game.
 $game_url = 'http://www.stfc.it';
