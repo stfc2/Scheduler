55a97a0643f17d7f01e1399a899cb9477c93d471
merry75 <kirirur@gmail.com>
2013-03-02 10:55:49 +0000

    Removed job "Exausted Colony planets"; not needed anymore with the new Settlers management.
    Replaced tab characters with spaces.

--- six_hours.php
+++ six_hours.php
@@ -21,8 +21,8 @@
 */
 
 
-// ########################################################################################
-// ########################################################################################
+// #######################################################################################
+// #######################################################################################
 // Startup Config
 
 // include game definitions, path url and so on
@@ -47,8 +47,8 @@ include('commons.php');
 include('NPC_BOT.php');
 
 
-// ########################################################################################
-// ########################################################################################
+// #######################################################################################
+// #######################################################################################
 // Init
 
 $starttime = ( microtime() + time() );
@@ -105,226 +105,65 @@ $sdl->finish_job('Mine Job'); // terminates the timer
 
 
 
+// #######################################################################################
+// #######################################################################################
 // Ok, now we try to update the user_max_colo
 $sdl->start_job('Recalculate colony ship limits');
 
 $sql = 'SELECT user_points FROM user ORDER BY user_points DESC LIMIT 30,1';
 if(!$limit = $db->queryrow($sql)) {
-	$sdl->log('<b>Error:</b> Could not query user points data! CONTINUED');
-	$limit['user_points'] = 2000;
+    $sdl->log('<b>Error:</b> Could not query user points data! CONTINUED');
+    $limit['user_points'] = 2000;
 }
 // Are there "big" players present?
 elseif($limit['user_points'] <= 2000) {
-	$limit['user_points'] = 2000;
+    $limit['user_points'] = 2000;
 }
 
 // Who is ABOVE the threshold can have only five colony ship at a time!!!
 $sql = 'UPDATE user SET user_max_colo = 5 WHERE user_points > '.$limit['user_points'];
 if(!$db->query($sql))
-	$sdl->log('<b>Error:</b> Cannot set user_max_colo to 5! CONTINUED');
+    $sdl->log('<b>Error:</b> Cannot set user_max_colo to 5! CONTINUED');
 
 //Who is equal to or smaller than the threshold, can do as many colony ship as he want!!!
 $sql = 'UPDATE user SET user_max_colo = 0 WHERE user_points <= '.$limit['user_points'];
 if(!$db->query($sql))
-	$sdl->log('<b>Error:</b> Cannot set user_max_colo to 0! CONTINUED');
+    $sdl->log('<b>Error:</b> Cannot set user_max_colo to 0! CONTINUED');
 
 $sdl->finish_job('Recalculate colony ship limits');
 
 
 
-// 31/10/08 - AC After 3 months owned by Settlers, planets returns inhabitated and becomes of D class.
-$sdl->start_job('Exausted Colony planets');
-
-$today = time();
-$oneWeek = (7 * 24 * 60 * 60);
-$threeMonths = $oneWeek * 12;
-$planet_type = 'd';
-
-// Select ALL exausted planets
-$sql = 'SELECT planet_id FROM planets
-        WHERE planet_owner = '.INDEPENDENT_USERID.' AND
-              '.$today.'  - planet_owned_date > '.$threeMonths;
-
-if(($exausted_planets = $db->queryrowset($sql)) === false) {
-	$sdl->log('<b>Error:</b> Could not query exausted planets!');
-}
-
-// Remove them to the settlers
-$sql = 'UPDATE planets SET planet_name = "Inesplorato",
-                           planet_type = "'.$planet_type.'",
-                           planet_owner = 0,
-                           planet_owner_enum = 0,
-                           planet_points = 0,
-                           research_1 = 0,
-                           research_2 = 0,
-                           research_3 = 0,
-                           research_4 = 0,
-                           research_5 = 0,
-                           resource_1 = 0,
-                           resource_2 = 0,
-                           resource_3 = 0,
-                           resource_4 = 0,
-                           add_1 = 0,
-                           add_2 = 0,
-                           add_3 = 0,
-                           add_4 = 0,
-                           max_resources = 0,
-                           max_worker = 0,
-                           max_units = 0,
-                           min_security_troops = 0,
-                           planet_insurrection_time = 0,
-                           building_1 = 0,
-                           building_2 = 0,
-                           building_3 = 0,
-                           building_4 = 0,
-                           building_5 = 0,
-                           building_6 = 0,
-                           building_7 = 0,
-                           building_8 = 0,
-                           building_9 = 0,
-                           building_10 = 0,
-                           building_11 = 0,
-                           building_12 = 0,
-                           building_13 = 0,
-                           unit_1 = 0,
-                           unit_2 = 0,
-                           unit_3 = 0,
-                           unit_4 = 0,
-                           unit_5 = 0,
-                           unit_6 = 0,
-                           workermine_1 = 0,
-                           workermine_2 = 0,
-                           workermine_3 = 0,
-                           catresearch_1 = 0,
-                           catresearch_2 = 0,
-                           catresearch_3 = 0,
-                           catresearch_4 = 0,
-                           catresearch_5 = 0,
-                           catresearch_6 = 0,
-                           catresearch_7 = 0,
-                           catresearch_8 = 0,
-                           catresearch_9 = 0,
-                           catresearch_10 = 0,
-                           unittrainid_1 = 0,
-                           unittrainid_2 = 0,
-                           unittrainid_3 = 0,
-                           unittrainid_4 = 0,
-                           unittrainid_5 = 0,
-                           unittrainid_6 = 0,
-                           unittrainid_7 = 0,
-                           unittrainid_8 = 0,
-                           unittrainid_9 = 0,
-                           unittrainid_10 = 0,
-                           unittrainnumber_1 = 0,
-                           unittrainnumber_2 = 0,
-                           unittrainnumber_3 = 0,
-                           unittrainnumber_4 = 0,
-                           unittrainnumber_5 = 0,
-                           unittrainnumber_6 = 0,
-                           unittrainnumber_7 = 0,
-                           unittrainnumber_8 = 0,
-                           unittrainnumber_9 = 0,
-                           unittrainnumber_10 = 0,
-                           unittrainnumberleft_1 = 0,
-                           unittrainnumberleft_2 = 0,
-                           unittrainnumberleft_3 = 0,
-                           unittrainnumberleft_4 = 0,
-                           unittrainnumberleft_5 = 0,
-                           unittrainnumberleft_6 = 0,
-                           unittrainnumberleft_7 = 0,
-                           unittrainnumberleft_8 = 0,
-                           unittrainnumberleft_9 = 0,
-                           unittrainnumberleft_10 = 0,
-                           unittrainendless_1 = 0,
-                           unittrainendless_2 = 0,
-                           unittrainendless_3 = 0,
-                           unittrainendless_4 = 0,
-                           unittrainendless_5 = 0,
-                           unittrainendless_6 = 0,
-                           unittrainendless_7 = 0,
-                           unittrainendless_8 = 0,
-                           unittrainendless_9 = 0,
-                           unittrainendless_10 = 0,
-                           unittrain_actual = 0,
-                           unittrainid_nexttime = 0,
-                           unittrain_error = 0,
-                           building_queue = 0,
-                           planet_altname = "",
-                           planet_surrender = 0
-        WHERE planet_owner = '.INDEPENDENT_USERID.' AND
-              '.$today.'  - planet_owned_date > '.$threeMonths;
-
-if(!$db->query($sql))
-{
-	$sdl->log('<b>Error:</b> Could not remove exausted planet to the settlers!');
-}
-
-$planets_ids = array();
-$planets_num = 0;
-
-foreach($exausted_planets as $i => $cur_planet) {
-	$planets_ids[] = $cur_planet['planet_id'];
-	$planets_num++;
-
-	// Recalculate rateo for each planet
-	$rateo_1 = round(($PLANETS_DATA[$planet_type][0] + ((400 - mt_rand(0, 800))*0.001)), 2);
-	if($rateo_1 < 0.1) $rateo_1 = 0.1;
-	$rateo_2 = round(($PLANETS_DATA[$planet_type][1] + ((350 - mt_rand(0, 700))*0.001)), 2);
-	if($rateo_2 < 0.1) $rateo_2 = 0.1;
-	$rateo_3 = round(($PLANETS_DATA[$planet_type][2] + ((300 - mt_rand(0, 600))*0.001)), 2);
-	if($rateo_3 < 0.1) $rateo_3 = 0.1;
-	$rateo_4 = $PLANETS_DATA[$planet_type][3];
-
-	$sql = 'UPDATE planets SET rateo_1 = '.$rateo_1.',
-	                           rateo_2 = '.$rateo_2.',
-	                           rateo_3 = '.$rateo_3.',
-	                           rateo_4 = '.$rateo_4.'
-	        WHERE planet_id = '.$cur_planet['planet_id'];
-
-	if(!$db->query($sql))
-	{
-		$sdl->log('<b>Error:</b> Could set new rateo values to planet <b>#'.$cur_planet['planet_id'].'</b>');
-	}
-}
-
-if($planets_num != 0) {
-	$sql = 'DELETE FROM planet_details WHERE planet_id IN ('.implode(',', $planets_ids).') AND log_code = 300';
-	if(!$db->query($sql)) {
-		$sdl->log('<b>Error:</b> Could not delete settlers moods!');
-	}
-
-	$sdl->log('Exausted Colony planet: <b>'.$planets_num.'</b> returned uninhabited');
-}
-$sdl->finish_job('Exausted Colony planets');
-
-
-
+// #######################################################################################
+// #######################################################################################
 // Check of Settler Planets OMG!!! LOT OF TIME USED!!!
 $sdl->start_job('Colony DB checkup');
 $sql = 'SELECT planet_id FROM planets WHERE planet_owner = '.INDEPENDENT_USERID;
 $planets_restored = 0;
 $settlers_planets = $db->query($sql);
 while($fetch_planet=$db->fetchrow($settlers_planets)) {
-	$sql='SELECT * FROM planet_details WHERE planet_id = '.$fetch_planet['planet_id'].' AND log_code = 300';
-	if(!$db->queryrow($sql)) {
-		$sdl->log('Colony Exception: planet '.$fetch_planet['planet_id'].' with missing moods information! Restoring with default data...');
-		$sql='INSERT INTO planet_details SET planet_id  = '.$fetch_planet['planet_id'].', 
-		                  user_id = '.INDEPENDENT_USERID.',
-		                  log_code   = 300, 
-		                  timestamp  = '.time();
-		//$sdl->log('Colony SQL: '.$sql);
-		if(!$db->query($sql))
-		{
-			$sdl->log('<b>Error:</b> Bot: Could not insert default colony moods data!');
-		}
-		$planets_restored++;
-	}
+    $sql='SELECT * FROM planet_details WHERE planet_id = '.$fetch_planet['planet_id'].' AND log_code = 300';
+    if(!$db->queryrow($sql)) {
+        $sdl->log('Colony Exception: planet '.$fetch_planet['planet_id'].' with missing moods information! Restoring with default data...');
+        $sql='INSERT INTO planet_details SET planet_id  = '.$fetch_planet['planet_id'].', 
+                          user_id = '.INDEPENDENT_USERID.',
+                          log_code   = 300, 
+                          timestamp  = '.time();
+        //$sdl->log('Colony SQL: '.$sql);
+        if(!$db->query($sql))
+        {
+            $sdl->log('<b>Error:</b> Bot: Could not insert default colony moods data!');
+        }
+        $planets_restored++;
+    }
 }
 if($planets_restored != 0) $sdl->log('Colony Report: Restored '.$planets_restored.' default planets mood data');
 $sdl->finish_job('Colony DB checkup');
 
 
 
+// #######################################################################################
+// #######################################################################################
 // Check of miners available on Borg planets
 $sdl->start_job('Check miners on Borg planets');
 $mines_upgraded = 0;
@@ -337,78 +176,78 @@ $borg -> LoadNPCUserData(BORG_USERID);
 $sql = 'SELECT * FROM planets WHERE planet_owner = '.BORG_USERID;
 $borg_planets = $db->query($sql);
 while($planet=$db->fetchrow($borg_planets)) {
-	// If we have at least a workers slot
-	if($planet['resource_4'] > 100) {
-		$mine = 0;
-		$max_reached = 0;
-		$miners_updated = false;
-		$miners[0] = $planet['workermine_1'];
-		$miners[1] = $planet['workermine_2'];
-		$miners[2] = $planet['workermine_3'];
-		$workers = $planet['resource_4'];
-		$mines_level[0] = $planet['building_2'];
-		$mines_level[1] = $planet['building_3'];
-		$mines_level[2] = $planet['building_4'];
+    // If we have at least a workers slot
+    if($planet['resource_4'] > 100) {
+        $mine = 0;
+        $max_reached = 0;
+        $miners_updated = false;
+        $miners[0] = $planet['workermine_1'];
+        $miners[1] = $planet['workermine_2'];
+        $miners[2] = $planet['workermine_3'];
+        $workers = $planet['resource_4'];
+        $mines_level[0] = $planet['building_2'];
+        $mines_level[1] = $planet['building_3'];
+        $mines_level[2] = $planet['building_4'];
 
-		while($workers > 100 && $max_reached < 3) {
-			// If there is space for new workers
-			if($miners[$mine] < (($mines_level[$mine]*100)+100)) {
-				$miners[$mine]+=100;
-				$workers-=100;
-				$miners_updated = true;
-			}
-			// There is no space available, perhaps we can increase the mine level?
-			else {
-				$max_reached++;
-			}
-			$mine++;
-			if($mine > 2)
-				$mine = 0;
-		}
+        while($workers > 100 && $max_reached < 3) {
+            // If there is space for new workers
+            if($miners[$mine] < (($mines_level[$mine]*100)+100)) {
+                $miners[$mine]+=100;
+                $workers-=100;
+                $miners_updated = true;
+            }
+            // There is no space available, perhaps we can increase the mine level?
+            else {
+                $max_reached++;
+            }
+            $mine++;
+            if($mine > 2)
+                $mine = 0;
+        }
 
-		// If we exit from the while because there isn't space available on the mines
-		if($max_reached) {
-			// Search the mine with lowest level
-			$min = $mines_level[0];
-			$mine = 1;
-			for($i = 1;$i < 3;$i++)
-				if($mines_level[$i] < $min)
-				{
-					$min = $mines_level[$i];
-					$mine = $i + 1;
-				}
+        // If we exit from the while because there isn't space available on the mines
+        if($max_reached) {
+            // Search the mine with lowest level
+            $min = $mines_level[0];
+            $mine = 1;
+            for($i = 1;$i < 3;$i++)
+                if($mines_level[$i] < $min)
+                {
+                    $min = $mines_level[$i];
+                    $mine = $i + 1;
+                }
 
-			// Start to build a new mine level
-			$res = $borg->StartBuild($ACTUAL_TICK,$mine,$planet);
-			if($res == BUILD_ERR_ENERGY)
-				$res = $borg->StartBuild($ACTUAL_TICK,4,$planet);
-		}
+            // Start to build a new mine level
+            $res = $borg->StartBuild($ACTUAL_TICK,$mine,$planet);
+            if($res == BUILD_ERR_ENERGY)
+                $res = $borg->StartBuild($ACTUAL_TICK,4,$planet);
+        }
 
-		if($miners_updated) {
-			$sql='UPDATE planets SET
-			             workermine_1 = '.$miners[0].',
-			             workermine_2 = '.$miners[1].',
-			             workermine_3 = '.$miners[2].',
-			             resource_4 = '.$workers.'
-			      WHERE planet_id = '.$planet['planet_id'];
+        if($miners_updated) {
+            $sql='UPDATE planets SET
+                         workermine_1 = '.$miners[0].',
+                         workermine_2 = '.$miners[1].',
+                         workermine_3 = '.$miners[2].',
+                         resource_4 = '.$workers.'
+                  WHERE planet_id = '.$planet['planet_id'];
 
-			$sdl->log('Mines workers increased to: '.$miners[0].'/'.$miners[1].'/'.$miners[2].' on Borg planet: <b>#'.$planet['planet_id'].'</b>');
+            $sdl->log('Mines workers increased to: '.$miners[0].'/'.$miners[1].'/'.$miners[2].' on Borg planet: <b>#'.$planet['planet_id'].'</b>');
 
-			$mines_upgraded++;
+            $mines_upgraded++;
 
-			if(!$db->query($sql)) {
-				$sdl->log('<b>Error:</b> could not update mines workers!');
-			}
-		}
-	}
+            if(!$db->query($sql)) {
+                $sdl->log('<b>Error:</b> could not update mines workers!');
+            }
+        }
+    }
 }
 if($mines_upgraded != 0) $sdl->log('Upgraded <b>'.$mines_upgraded.'</b> mines on Borg planets');
 $sdl->finish_job('Check miners on Borg planets');
 
 
 
-// ########################################################################################
-// ########################################################################################
+// #######################################################################################
+// #######################################################################################
 // Quit and close log
 
 $db->close();
